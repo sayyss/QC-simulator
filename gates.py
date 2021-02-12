1d5e32d39fc83e4dfba4576c872b26333a77a47e
sayamss <sawaisayam@gmail.com>
2021-02-12 23:01:44 +0530

    Added CNOT gate

--- gates.py
+++ gates.py
@@ -17,5 +17,6 @@ cnot = np.array([[1,0,0,0],
                 [0,1,0,0],
                 [0,0,0,1],
                 [0,0,1,0]])
-# Rotation gate
-
+# Projection gatse
+p0x0 = np.array([[1,0],[0,0]])
+p1x1 = np.array([[0,0],[0,1]])
--- simulator.py
+++ simulator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from gates import h_gate,x_gate, iden, cnot
+from gates import h_gate,x_gate, iden, cnot,p0x0,p1x1
 import random
 
 class Simulator():
@@ -47,11 +47,31 @@ class Simulator():
         return result
 
     def cnot_operator(self,gate,t_qubits):
-        if self.n_qubits > 2:
-            return
-        else:
-            return cnot
-    
+
+        c_qubit = t_qubits[0]
+        t_qubit = t_qubits[1]
+
+        temp_p0x0, temp_p1x1 = p0x0,p1x1
+        g_1 = g_2 = np.array([1])
+
+        for i in range(self.n_qubits):
+
+            if i == c_qubit:
+                g_1 = np.kron(g_1,temp_p0x0)
+                g_2 = np.kron(g_2,temp_p1x1)
+            
+            if i == t_qubit:
+                g_1 = np.kron(g_1,iden)
+                g_2 = np.kron(g_2,x_gate)
+            
+            else:
+                g_1 = np.kron(g_1,iden)
+                g_2 = np.kron(g_2,iden)
+        
+        operator = g_1 + g_2
+
+        return operator
+        
     def measure(self):
 
         prob = np.abs(self.s_vector)**2
